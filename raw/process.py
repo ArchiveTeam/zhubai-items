365afdd94ec463298268f328c301fcdb8793eae3
arkiver <arkiver@protonmail.com>
2025-03-16 11:30:53 +0100

    process new list.

--- raw/process.py
+++ raw/process.py
@@ -1,13 +1,17 @@
+import html
 import re
 import sys
+import urllib.parse
 
 
 def main(filepath: str):
     items = set()
     with open(filepath, 'r') as f:
         for line in f:
+            line = html.unescape(line)
+            line = urllib.parse.unquote(line)
             for s in re.findall(r'([^\./]+)\.zhubai\.love', line):
-                items.add('site:'+s)
+                items.add('site:'+s.lower())
     with open(filepath+'.items', 'w') as f:
         f.write('\n'.join(sorted(items))+'\n')
 
